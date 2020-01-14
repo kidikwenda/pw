29c6343d168a6aea7bf6c2c2b09eb1a36e21c347
Adilson Quifucussa <adilson.quifucussa@TISTECH.LAN>
2020-01-14 09:06:13 +0100

    corrigi o sw.js para enviar as notificações

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('teste message', {});
-})  
+self.addEventListener('push', (e) => {
+    var options = {
+        body: 'Notificação gerada através de push',
+        icon: './push_logo.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore',
+                title: 'Explorar',
+                icon: './check.png'
+            },
+            {
+                action: 'close',
+                title: 'Fechar',
+                icon: './check.png'
+            }
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('teste message', options)
+    );
+});
