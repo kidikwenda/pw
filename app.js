3710557342b62f78a37cecf4551581b89845d030
Adilson Quifucussa <adilson.quifucussa@TISTECH.LAN>
2020-01-16 16:10:45 +0100

    site na heroku

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/javascript');
-  res.end('Hello World2\n');
-});
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type':'text/html'});
+  
+  fs.readFile('./index.html', function(err, data){
+    if (err) {
+      response.writeHead(404);
+      respone.write('Whoops! File not found!');
+  } else {
+      response.write(data);
+  }
+  response.end();
+  }
+)}).listen(port);
 
-server.listen(port, () => {
-  console.log(`Server running`);
-}); 
+console.log(`Server running`);
